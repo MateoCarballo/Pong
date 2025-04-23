a308204c6297c9666b70cad4dbc7a61df9867c91
MateoPortatil <mateocarballo2@gmail.com>
2025-04-23 21:17:30 +0200

    faltan cosas

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,8 +7,20 @@ public class GameManager : MonoBehaviour
     public int leftPlayerScore, rightPlayerScore;
     public TMPro.TextMeshProUGUI playerLeftScoreText, playerRightScoreText;
 
+    public int maxScore = 2;
+    public string winner; //This is used to store the winner of the game
 
+    public Boolean pvp;
 
+    private static GameManager _instance;    //This is used to make sure there is only one manager in the scene
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+
+        }
+    }
     // This method is called when the ball enters the goal area (the trigger collider)
     public void GoalScored(bool isPlayerLeft)
     {
@@ -21,7 +34,31 @@ public class GameManager : MonoBehaviour
             rightPlayerScore++;
             playerRightScoreText.text = rightPlayerScore.ToString();
         }
+        CheckWinner();
         ReserPositions();
+    }
+
+    private void CheckWinner()
+    {
+        if(leftPlayerScore >= maxScore)
+        {
+            Debug.Log("Player 1 Wins!");
+            winner = "Player 1";
+        } else if (rightPlayerScore >= maxScore && pvp)
+        {
+            Debug.Log("Player 2 Wins!");
+            winner = "Player 2";
+        }
+        else if (rightPlayerScore >= maxScore && !pvp)
+        {
+            Debug.Log("CPU Wins!");
+            winner = "CPU";
+        }
+    }
+
+    public String GetWinner()
+    {
+        return winner;
     }
 
     // This method is called to reset the ball and players to their starting positions
---
+++ Assets/Script/WinnerScene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinnerScene : MonoBehaviour
+
+{
+    private GameManager gameManager;
+
+}
